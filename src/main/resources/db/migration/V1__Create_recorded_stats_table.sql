0f4c397b53ed600a577fe11d4fbe237e7ceb0e9f
Patryk Koryzna <patryk@koryzna.com>
2021-02-08 21:50:28 +0100

    create schema for app instead of using public
    normally I wouldn't modify a migration in place - one should always create a new migration - but this wasn't deployed anywhere yet

--- src/main/resources/db/migration/V1__Create_recorded_stats_table.sql
+++ src/main/resources/db/migration/V1__Create_recorded_stats_table.sql
@@ -1,8 +1,10 @@
-CREATE TABLE recorded_stats (
+CREATE SCHEMA stats_app;
+
+CREATE TABLE stats_app.recorded_stats (
     machine_name varchar not null,
     stat_name varchar not null,
     machine_timestamp_unix bigint not null,
     stat_value double precision not null,
     insert_timestamp timestamp with time zone not null,
     PRIMARY KEY (machine_name, stat_name, machine_timestamp_unix)
-)
+);
