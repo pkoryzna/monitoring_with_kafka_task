ec684438a7aebe301bca808a2845143e28229686
Patryk Koryzna <patryk@koryzna.com>
2021-02-08 20:51:51 +0100

    add minimal schema

---
+++ src/main/resources/db/migration/V1__Create_recorded_stats_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE recorded_stats (
+    machine_name varchar not null,
+    stat_name varchar not null,
+    machine_timestamp_unix bigint not null,
+    stat_value double precision not null,
+    insert_timestamp timestamp with time zone not null,
+    PRIMARY KEY (machine_name, stat_name, machine_timestamp_unix)
+)
